9da8206e8ed80826cef011658cb9a73a03dd7b84
David Honour <david@concertdaw.co.uk>
2018-09-15 10:34:27 +0100

    Add video playback

--- glued.js
+++ glued.js
@@ -19,6 +19,30 @@ class StaticImage {
     }
 }
 
+class Video {
+    constructor(src, container, next) {
+        this.src = src;
+        this.container = container;
+        this.next = next;
+    }
+
+    show(ready) {
+        var v = document.createElement('video');
+        var c = this.container;
+        v.src = this.src;
+        v.autoplay = true;
+        v.classList.add('fadein');
+        v.addEventListener("animationend", _ => v.classList.remove('fadein'), {once: true});
+        c.appendChild(v);
+        ready();
+        var cb = _ => this.next.show(function () {
+            v.classList.add('fadeout');
+            v.addEventListener("animationend", _ => c.removeChild(v));
+        });
+        v.addEventListener("ended", cb, {once: true});
+    }
+}
+
 class EndStop {
     show(ready) {
     }
@@ -32,7 +56,9 @@ function wholeSet() {
     var content = document.getElementById('content');
     var endstop = new EndStop();
     var finalLogo = new StaticImage('forayslogo.svg', backdrop, endstop);
-    var coil = new StaticImage('Coil/AnimAtomThingy.svg', content, finalLogo);
+    var mexico = new Video('NewMexico.mp4', content, finalLogo);
+    var preMexLogo = new StaticImage('forayslogo.svg', backdrop, mexico);
+    var coil = new StaticImage('Coil/AnimAtomThingy.svg', content, preMexLogo);
     coil.show(noop);
 }
 
