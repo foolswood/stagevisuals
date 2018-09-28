0fec0efbd567f508a1df2cb9b941d99d98a2ef1b
David Honour <david@concertdaw.co.uk>
2018-09-28 21:49:57 +0100

    Add more stuff

--- glued.js
+++ glued.js
@@ -30,10 +30,20 @@ class StaticImage {
 }
 
 class Video {
-    constructor(src, container, next) {
+    constructor(src, container, next, loop, fade) {
         this.src = src;
         this.container = container;
         this.next = next;
+        if (loop === undefined) {
+            this.loop = false;
+        } else {
+            this.loop = loop;
+        }
+        if (fade === undefined) {
+            this.fade = true;
+        } else {
+            this.fade = fade;
+        }
     }
 
     show(ready) {
@@ -41,17 +51,26 @@ class Video {
         var c = this.container;
         v.src = this.src;
         v.autoplay = true;
+        v.loop = this.loop;
         v.width = 1920;
         v.height = 1080;
         v.classList.add('fadein');
         v.addEventListener("animationend", _ => v.classList.remove('fadein'), {once: true});
         c.appendChild(v);
         ready();
+        var fade = this.fade;
         var cb = _ => this.next.show(function () {
-            v.classList.add('fadeout');
-            v.addEventListener("animationend", _ => c.removeChild(v));
+            v.removeEventListener("ended", cb, {once: true});
+            window.removeEventListener("keypress", cb);
+            if (fade) {
+                v.classList.add('fadeout');
+                v.addEventListener("animationend", _ => c.removeChild(v));
+            } else {
+                c.removeChild(v);
+            }
         });
         v.addEventListener("ended", cb, {once: true});
+        window.addEventListener("keypress", cb, {once: true});
     }
 }
 
@@ -88,18 +107,25 @@ function wholeSet() {
     var backdrop = document.getElementById('backdrop');
     var content = document.getElementById('content');
     var endstop = new EndStop();
+
     var finalLogo = new StaticImage('forayslogo.svg', backdrop, endstop);
     var mexico = new Video('NewMexico.mp4', content, finalLogo);
     var preMexLogo = new StaticImage('forayslogo.svg', backdrop, mexico);
+    // P&P
     var coil = new StaticImage('Coil/AnimAtomThingy.svg', content, preMexLogo);
-    var preCoilLogo = new StaticImage('forayslogo.svg', backdrop, coil);
+    var preCoilLogo = new StaticImage('epcover.png', backdrop, coil);
     var clarity = new SvgAnim('Clarity/HillsAnim.svg', content, preCoilLogo);
     var preClarity = new StaticImage('blackout.svg', backdrop, clarity);
     var answer = new Video('Answer.ogg', content, preClarity);
     var preAnswer = new StaticImage('epcover.png', backdrop, answer, 2466);
     var churn = new Video('Churn.mp4', content, preAnswer);
     var preChurn = new StaticImage('forayslogo.svg', backdrop, churn);
-    preChurn.show(noop);
+    // Burn
+    // Olive
+    // NWO
+    var evDaydream = new SvgAnim('EV/DDRTickerAnim.svg', backdrop, preChurn);
+    var preEv = new Video('EV/rainscenes.ogg', content, evDaydream, true, false);
+    preEv.show(noop);
 }
 
 document.onload = wholeSet;
