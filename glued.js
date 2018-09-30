c6a39b58bdea10b664ed2736d6dc401f9236ebf7
David Honour <david@concertdaw.co.uk>
2018-09-30 08:12:44 +0100

    Add in cult radio logo

--- glued.js
+++ glued.js
@@ -30,19 +30,27 @@ class StaticImage {
 }
 
 class Video {
-    constructor(src, container, next, loop, fade) {
+    constructor(src, container, next, opts) {
         this.src = src;
         this.container = container;
         this.next = next;
-        if (loop === undefined) {
+        if (opts === undefined) {
+            opts = {};
+        }
+        if (opts.loop === undefined) {
             this.loop = false;
         } else {
-            this.loop = loop;
+            this.loop = opts.loop;
         }
-        if (fade === undefined) {
-            this.fade = true;
+        if (opts.fadein === undefined) {
+            this.fadein = true;
         } else {
-            this.fade = fade;
+            this.fadein = opts.fadein;
+        }
+        if (opts.fadeout === undefined) {
+            this.fadeout = true;
+        } else {
+            this.fadeout = opts.fadeout;
         }
     }
 
@@ -54,17 +62,25 @@ class Video {
         v.loop = this.loop;
         v.width = 1920;
         v.height = 1080;
-        var fade = this.fade;
-        if (this.fade) {
+        if (this.fadein) {
             v.classList.add('fadein');
-            v.addEventListener("animationend", _ => v.classList.remove('fadein'), {once: true});
+            c.appendChild(v);
+            v.addEventListener(
+                "animationend",
+                function () {
+                    v.classList.remove('fadein');
+                    ready();
+                },
+                {once: true});
+        } else {
+            c.appendChild(v);
+            ready();
         }
-        c.appendChild(v);
-        ready();
+        var fadeout = this.fadeout;
         var cb = _ => this.next.show(function () {
             v.removeEventListener("ended", cb, {once: true});
-            window.removeEventListener("keypress", cb);
-            if (fade) {
+            window.removeEventListener("keypress", cb, {once: true});
+            if (fadeout) {
                 v.classList.add('fadeout');
                 v.addEventListener("animationend", _ => c.removeChild(v));
             } else {
@@ -77,25 +93,41 @@ class Video {
 }
 
 class SvgAnim {
-    constructor(src, container, next) {
+    constructor(src, container, next, opts) {
         this.src = src;
         this.container = container;
         this.next = next;
+        if (opts === undefined) {
+            opts = {};
+        }
+        if (opts.fade === undefined) {
+            this.fade = opts.fade;
+        }
     }
 
     show(ready) {
         var i = document.createElement('object');
         var c = this.container;
         i.data = this.src;
-        c.appendChild(i);
-        i.classList.add('fadein');
-        i.addEventListener("animationend", function() {
-            i.classList.remove('fadein');
+        if (this.fade) {
+            i.classList.add('fadein');
+            c.appendChild(i);
+            i.addEventListener("animationend", function() {
+                i.classList.remove('fadein');
+                ready();
+                }, {once: true});
+        } else {
+            c.appendChild(i);
             ready();
-        }, {once: true});
+        }
+        var fadeout = this.fade;
         var cb = _ => this.next.show(function () {
-            i.classList.add('fadeout');
-            i.addEventListener("animationend", _ => c.removeChild(i));
+            if (fadeout) {
+                i.classList.add('fadeout');
+                i.addEventListener("animationend", _ => c.removeChild(i));
+            } else {
+                c.removeChild(i);
+            }
         });
         window.addEventListener("keypress", cb, {once: true});
     }
@@ -134,7 +166,9 @@ function wholeSet() {
     // P&P
     var coil = new StaticImage('Coil/AnimAtomThingy.svg', content, preMexLogo);
     var preCoilLogo = new StaticImage('epcover.png', backdrop, coil);
-    var clarity = new SvgAnim('Clarity/HillsAnim.svg', content, preCoilLogo);
+    var warning = new Video('Warning.ogg', content, preCoilLogo);
+    var preWarning = new StaticImage('epcover.png', backdrop, warning);
+    var clarity = new SvgAnim('Clarity/HillsAnim.svg', content, preWarning);
     var preClarity = new StaticImage('blackout.svg', backdrop, clarity);
     var answer = new Video('Answer.ogg', content, preClarity);
     var preAnswer = new StaticImage('epcover.png', backdrop, answer, 2466);
@@ -145,16 +179,18 @@ function wholeSet() {
     // NWO
     var preNwo = new StaticImage('blackout.svg', backdrop, preChurn);
 
-    var evOutro2 = new Video('EV/sundown_late.mp4', content, preNwo, false, false);
-    var evOutro1 = new Video('EV/sundown_early.mp4', content, evOutro2, false, false);
-    var evChorus3 = new Video('EV/sunrise.ogg', backdrop, evOutro1, false, false);
-    var evDaydream = new SvgAnim('EV/DDRTickerAnim.svg', content, evChorus3);
-    var evChorus2 = new Video('EV/flickery_sundown.mp4', backdrop, evDaydream, false, false);
+    var evOutro2 = new Video('EV/sundown_late.mp4', content, preNwo, {fadein: false, fadeout: false});
+    var evOutro1 = new Video('EV/sundown_early.mp4', backdrop, evOutro2, {fadein: false, fadeout: false});
+    var evChorus3 = new Video('EV/sunrise.ogg', content, evOutro1, {fadeout: false});
+    var evDaydream = new SvgAnim('EV/DDRTickerAnim.svg', backdrop, evChorus3, {fade: false});
+    var evChorus2 = new Video('EV/flickery_sundown.mp4', content, evDaydream);
     var evVerse2 = new Placeholder('Ivan futurescape', backdrop, evChorus2);
     var evChorus1 = new Video('EV/flickery_sundown.mp4', content, evVerse2);
     var evVerse1 = new Placeholder('Ivan logo', backdrop, evChorus1);
-    var preEv = new Video('EV/rainscenes.ogg', content, evVerse1, true, false);
-    preEv.show(noop);
+    var preEv = new Video('EV/rainscenes.ogg', content, evVerse1, {loop: true, fadeout: false});
+    
+    var cultRadio = new StaticImage('support/cultradio.jpg', backdrop, preEv);
+    cultRadio.show(noop);
 }
 
 document.onload = wholeSet;
